07c1acecc9463afd313aee0461e7fedccd28edde
Marcelo Freire Mendes <celinhofm@gmail.com>
2021-10-11 01:44:34 -0300

    Update index.js
    Identation

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ if (app.get('env' === 'development')) {
 }
 app.use(morgan('tiny'));
 app.use(express.json());
+
 app.use('/api/jogadores', jogadores);
 app.use('/', home);
+
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Server iniciado na porta: ${port}`));
 
