72b6f26f1d2b782044402b20d430ed75f4684be2
Marcelo Freire Mendes <celinhofm@gmail.com>
2021-10-24 17:01:57 -0300

    mudanca nome db

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('./startup/db')();
 require('./startup/config')(app);
 require('express-async-errors');
 
-console.log(process.env.port, process.env.YOUR_HOST);
+console.log(process.env);
 const port = process.env.port || 3000;
 const host = process.env.YOUR_HOST || '0.0.0.0';
 app.listen(port, host, () => console.log(`Server iniciado na porta: ${port}.`));
