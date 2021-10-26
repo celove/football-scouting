30c0296108fd871cca1d2e4fd0d7b51d2b8cd656
Marcelo Freire Mendes <celinhofm@gmail.com>
2021-10-26 00:04:00 -0300

    mudanca no metodo de criar artigo

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ require('./startup/db')();
 require('./startup/config')(app);
 require('express-async-errors');
 
-console.log(process.env);
 const port = process.env.PORT || 3000;
 const host = process.env.YOUR_HOST || '0.0.0.0';
 app.listen(port, host, () => console.log(`Server iniciado na porta: ${port} e host: ${host}.`));
